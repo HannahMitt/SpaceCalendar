fbe564a77f5f48c92089de6a12f5df81a9b45209
Hannah Mittelstaedt <hannahmittel@gmail.com>
2013-04-19 17:10:25 -0400

    HM - added UI test of Times Square calendar library

--- src/com/spaceappsto/spacecalendar/MainActivity.java
+++ src/com/spaceappsto/spacecalendar/MainActivity.java
@@ -2,7 +2,9 @@ package com.spaceappsto.spacecalendar;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 
@@ -17,6 +19,14 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		if(item.getItemId() == R.id.menu_timessquare){
+			startActivity(new Intent(this, SampleTimesSquareActivity.class));
+		}
+		return super.onOptionsItemSelected(item);
 	}
 
 }
---
+++ src/com/spaceappsto/spacecalendar/SampleTimesSquareActivity.java
@@ -0,0 +1,36 @@
+package com.spaceappsto.spacecalendar;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Toast;
+import com.squareup.timessquare.CalendarPickerView;
+import java.util.Calendar;
+import java.util.Date;
+
+import static android.widget.Toast.LENGTH_SHORT;
+
+public class SampleTimesSquareActivity extends Activity {
+  private static final String TAG = "SampleTimesSquareActivity";
+
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.calendar_picker);
+
+    Calendar nextYear = Calendar.getInstance();
+    nextYear.add(Calendar.YEAR, 1);
+
+    final CalendarPickerView calendar = (CalendarPickerView) findViewById(R.id.calendar_view);
+    calendar.init(new Date(), new Date(), nextYear.getTime());
+
+    findViewById(R.id.done_button).setOnClickListener(new OnClickListener() {
+      @Override public void onClick(View view) {
+        Log.d(TAG, "Selected time in millis: " + calendar.getSelectedDate().getTime());
+        String toast = "Selected: " + calendar.getSelectedDate().getTime();
+        Toast.makeText(SampleTimesSquareActivity.this, toast, LENGTH_SHORT).show();
+      }
+    });
+  }
+}
