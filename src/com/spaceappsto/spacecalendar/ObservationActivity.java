5762b265b9b55e0d2af9ca3c36b6dfb994242f29
Hannah Mittelstaedt <hannahmittel@gmail.com>
2013-04-20 19:35:47 -0400

    Added launch to simbad button

--- src/com/spaceappsto/spacecalendar/ObservationActivity.java
+++ src/com/spaceappsto/spacecalendar/ObservationActivity.java
@@ -1,7 +1,10 @@
 package com.spaceappsto.spacecalendar;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import com.spaceappsto.spacecalendar.network.Observation;
@@ -10,12 +13,14 @@ public class ObservationActivity extends Activity {
 
 	public static final String BUNDLE_KEY = "key_obs";
 	
+	private Observation observation;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.observation_view);
 		
-		Observation observation = getIntent().getParcelableExtra(BUNDLE_KEY);
+		observation = getIntent().getParcelableExtra(BUNDLE_KEY);
 		
 		((TextView)findViewById(R.id.target_name)).setText(observation.target.name);
 		
@@ -25,5 +30,12 @@ public class ObservationActivity extends Activity {
 		((TextView)findViewById(R.id.revolution)).setText("Revolution: " + observation.revolution);
 		((TextView)findViewById(R.id.time_window)).setText(observation.startTime + " - " + observation.finishTime);
 		((TextView)findViewById(R.id.satellite)).setText(observation.satellite.name);
+	}
+	
+	public void onSimbadClick(View view){
+		String url = "http://simbad.u-strasbg.fr/simbad/sim-id?Ident=" + observation.target.name;
+		Intent i = new Intent(Intent.ACTION_VIEW);
+		i.setData(Uri.parse(url));
+		startActivity(i);
 	}
 }
