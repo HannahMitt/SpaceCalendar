7ce6bc3660c09b98365215bbea1c1ac97781b560
Hannah Mittelstaedt <hannahmittel@gmail.com>
2013-04-20 17:58:19 -0400

    Added onClick to listview

--- src/com/spaceappsto/spacecalendar/SearchActivity.java
+++ src/com/spaceappsto/spacecalendar/SearchActivity.java
@@ -1,9 +1,15 @@
 package com.spaceappsto.spacecalendar;
 
 import com.spaceappsto.spacecalendar.adapter.SearchAdapter;
+import com.spaceappsto.spacecalendar.network.Satellite;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class SearchActivity extends Activity {
@@ -15,6 +21,14 @@ public class SearchActivity extends Activity {
 		
 		ListView listView = (ListView) findViewById(R.id.listview);
 		SearchAdapter searchAdapter = new SearchAdapter(this, 0, 0, CalendarActivity.satellites);
-		listView.setAdapter(searchAdapter);
+		//listView.setAdapter(searchAdapter);
+		listView.setAdapter(new ArrayAdapter<Satellite>(this, R.layout.search_cell, R.id.name, CalendarActivity.satellites));
+		listView.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+				startActivity(new Intent(SearchActivity.this, DayActivity.class));
+			}
+		});
 	}
 }
--- src/com/spaceappsto/spacecalendar/network/Satellite.java
+++ src/com/spaceappsto/spacecalendar/network/Satellite.java
@@ -4,21 +4,16 @@ import java.util.ArrayList;
 import java.util.Date;
 
 public class Satellite {
-	
+
 	public String url;
 	public boolean active;
 	public String name;
 	public int id;
-	
+
 	public ArrayList<Date> dates;
-	
-//    "url": "http://sci.esa.int/xmm-newton", 
-//    "active": true, 
-//    "link": {
-//        "url": "/api/satellites/xmm-newton", 
-//        "rel": "self"
-//    }, 
-//    "name": "XMM-Newton", 
-//    "id": 1
-//},
+
+	@Override
+	public String toString() {
+			return name + " " + super.toString();
+	}
 }
