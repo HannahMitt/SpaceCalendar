e10248c97f593570ee3bbdf735b24f4f6053028b
Hannah Mittelstaedt <hannahmittel@gmail.com>
2013-04-21 11:15:38 -0400

    First pass styling observation details

--- src/com/spaceappsto/spacecalendar/ObservationActivity.java
+++ src/com/spaceappsto/spacecalendar/ObservationActivity.java
@@ -1,5 +1,7 @@
 package com.spaceappsto.spacecalendar;
 
+import java.text.SimpleDateFormat;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
@@ -9,6 +11,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.spaceappsto.spacecalendar.utility.SatellitePicturesUtility;
+import com.squareup.timessquare.DotUtility;
 import com.squareup.timessquare.objects.Observation;
 
 public class ObservationActivity extends Activity {
@@ -23,17 +26,22 @@ public class ObservationActivity extends Activity {
 		setContentView(R.layout.observation_view);
 
 		observation = getIntent().getParcelableExtra(BUNDLE_KEY);
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
 
 		((ImageView) findViewById(R.id.satellite_pic)).setImageResource(SatellitePicturesUtility.getResourceIdForSatellite(observation.satellite.name));
+		TextView satText = (TextView) findViewById(R.id.satellite);
+		satText.setText(observation.satellite.name);
+		satText.setCompoundDrawablesWithIntrinsicBounds(DotUtility.getDotWithColorIndex(this, observation.satellite.id), null, null, null);
 
 		((TextView) findViewById(R.id.target_name)).setText(observation.target.name);
 
-		String raDec = "RA: " + observation.target.ra + " / DEC: " + observation.target.dec;
-		((TextView) findViewById(R.id.ra_dec)).setText(raDec);
+		((TextView) findViewById(R.id.ra)).setText(observation.target.ra);
+		((TextView) findViewById(R.id.dec)).setText(observation.target.dec);
 
 		((TextView) findViewById(R.id.revolution)).setText("Revolution: " + observation.revolution);
-		((TextView) findViewById(R.id.time_window)).setText(observation.start_time + " - " + observation.finish_time);
-		((TextView) findViewById(R.id.satellite)).setText(observation.satellite.name);
+		((TextView) findViewById(R.id.start_time)).setText(sdf.format(observation.start_time));
+		((TextView) findViewById(R.id.end_time)).setText(sdf.format(observation.finish_time));
 	}
 
 	public void onSimbadClick(View view) {
