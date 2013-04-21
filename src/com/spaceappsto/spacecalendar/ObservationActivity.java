dd35b164aeaf61197bfedfd1f453002ecf77476f
Hannah Mittelstaedt <hannahmittel@gmail.com>
2013-04-21 10:54:23 -0400

    Added static pictures for satellites

--- src/com/spaceappsto/spacecalendar/ObservationActivity.java
+++ src/com/spaceappsto/spacecalendar/ObservationActivity.java
@@ -5,34 +5,38 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.spaceappsto.spacecalendar.utility.SatellitePicturesUtility;
 import com.squareup.timessquare.objects.Observation;
 
 public class ObservationActivity extends Activity {
 
 	public static final String BUNDLE_KEY = "key_obs";
-	
+
 	private Observation observation;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.observation_view);
-		
+
 		observation = getIntent().getParcelableExtra(BUNDLE_KEY);
-		
-		((TextView)findViewById(R.id.target_name)).setText(observation.target.name);
-		
+
+		((ImageView) findViewById(R.id.satellite_pic)).setImageResource(SatellitePicturesUtility.getResourceIdForSatellite(observation.satellite.name));
+
+		((TextView) findViewById(R.id.target_name)).setText(observation.target.name);
+
 		String raDec = "RA: " + observation.target.ra + " / DEC: " + observation.target.dec;
-		((TextView)findViewById(R.id.ra_dec)).setText(raDec);
-		
-		((TextView)findViewById(R.id.revolution)).setText("Revolution: " + observation.revolution);
-		((TextView)findViewById(R.id.time_window)).setText(observation.start_time + " - " + observation.finish_time);
-		((TextView)findViewById(R.id.satellite)).setText(observation.satellite.name);
+		((TextView) findViewById(R.id.ra_dec)).setText(raDec);
+
+		((TextView) findViewById(R.id.revolution)).setText("Revolution: " + observation.revolution);
+		((TextView) findViewById(R.id.time_window)).setText(observation.start_time + " - " + observation.finish_time);
+		((TextView) findViewById(R.id.satellite)).setText(observation.satellite.name);
 	}
-	
-	public void onSimbadClick(View view){
+
+	public void onSimbadClick(View view) {
 		String url = "http://simbad.u-strasbg.fr/simbad/sim-id?Ident=" + observation.target.name;
 		Intent i = new Intent(Intent.ACTION_VIEW);
 		i.setData(Uri.parse(url));
---
+++ src/com/spaceappsto/spacecalendar/utility/SatellitePicturesUtility.java
@@ -0,0 +1,22 @@
+package com.spaceappsto.spacecalendar.utility;
+
+import com.spaceappsto.spacecalendar.R;
+
+public class SatellitePicturesUtility {
+
+	public static int getResourceIdForSatellite(String name) {
+		if ("Chandra".equalsIgnoreCase(name)) {
+			return R.drawable.chandra;
+		} else if ("INTEGRAL".equalsIgnoreCase(name)) {
+			return R.drawable.integral;
+		} else if ("NuSTAR".equalsIgnoreCase(name)) {
+			return R.drawable.nustar;
+		} else if ("RXTE".equalsIgnoreCase(name)) {
+			return R.drawable.rxte;
+		} else if ("suzaku".equalsIgnoreCase(name)) {
+			return R.drawable.suzaku;
+		} else {
+			return R.drawable.space_background;
+		}
+	}
+}
