7c7e0d516ed9ceb7b9c2605b2ee3e44f06a1f585
Hannah Mittelstaedt <hannahmittel@gmail.com>
2013-04-21 00:59:08 -0400

    Set ra/dec dialog to update values

--- src/com/spaceappsto/spacecalendar/SearchActivity.java
+++ src/com/spaceappsto/spacecalendar/SearchActivity.java
@@ -5,9 +5,12 @@ import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.spaceappsto.spacecalendar.adapter.SearchAdapter;
 import com.spaceappsto.spacecalendar.network.ObservationsHolder;
@@ -15,16 +18,26 @@ import com.spaceappsto.spacecalendar.network.ObservationsHolder;
 public class SearchActivity extends Activity {
 
 	private Dialog coordsDialog;
+	private TextView ra_text;
+	private TextView dec_text;
+	private String ra = "00:00:00.00";
+	private String dec = "+000:00:00.00";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.search);
 
+		setUpRaDec();
 		setUpListView();
 		setUpDialog();
 	}
 
+	private void setUpRaDec(){
+		ra_text = (TextView) findViewById(R.id.ra_text);
+		dec_text = (TextView) findViewById(R.id.dec_text);
+	}
+	
 	private void setUpListView() {
 		ListView listView = (ListView) findViewById(R.id.listview);
 		SearchAdapter searchAdapter = new SearchAdapter(this, 0, 0, ObservationsHolder.getObservations());
@@ -45,6 +58,25 @@ public class SearchActivity extends Activity {
 	private void setUpDialog() {
 		coordsDialog = new Dialog(this, android.R.style.Theme_Holo_Light_Dialog_NoActionBar);
 		coordsDialog.setContentView(R.layout.coords_dialog);
+		coordsDialog.findViewById(R.id.dialog_button).setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				ra = ((EditText)coordsDialog.findViewById(R.id.ra_1)).getText().toString() + ":";
+				ra += ((EditText)coordsDialog.findViewById(R.id.ra_2)).getText().toString() + ":";
+				ra += ((EditText)coordsDialog.findViewById(R.id.ra_3)).getText().toString() + ".";
+				ra += ((EditText)coordsDialog.findViewById(R.id.ra_4)).getText().toString();
+				
+				dec = ((EditText)coordsDialog.findViewById(R.id.dec_1)).getText().toString() + ":";
+				dec += ((EditText)coordsDialog.findViewById(R.id.dec_2)).getText().toString() + ":";
+				dec += ((EditText)coordsDialog.findViewById(R.id.dec_3)).getText().toString() + ".";
+				dec += ((EditText)coordsDialog.findViewById(R.id.dec_4)).getText().toString();
+				
+				ra_text.setText("RA " + ra);
+				dec_text.setText("DEC " + dec);
+				coordsDialog.dismiss();
+			}
+		});
 	}
 
 	public void onClickEditSearch(View view) {
